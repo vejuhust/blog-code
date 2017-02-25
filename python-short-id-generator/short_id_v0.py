55a7b0d5c6b42c603cb780db21cb0b0ffbe82a17
Wei Ye <vejuhust@gmail.com>
2017-02-26 02:56:16 +0800

    Add detect_collision.py along with convert methods in v0~v3

--- python-short-id-generator/short_id_v0.py
+++ python-short-id-generator/short_id_v0.py
@@ -2,6 +2,12 @@
 from uuid import uuid1
 from basehash import base62
 
+
 def generate_short_id():
     """ Short ID generator - v0: Base62 Encoded UUID1 """
     return base62().encode(uuid1().int)
+
+
+def convert_short_id(num):
+    """ Short ID converter - v0: Base62 Encoded UUID1 """
+    return base62().encode(num)
--- python-short-id-generator/short_id_v3.py
+++ python-short-id-generator/short_id_v3.py
@@ -2,9 +2,19 @@
 from uuid import uuid4
 from basehash import base62
 
+
 def generate_short_id():
     """ Short ID generator - v3: Shuffled XOR UUID4 """
     num = uuid4().int
+    mask_shuffle = 0x55555555555555555555555555555555
+    mask_half = (1 << 64) - 1
+    result = (num ^ (num >> 1)) & mask_shuffle
+    result = ((mask_half & (result >> 64)) << 1) | (mask_half & result)
+    return base62().encode(result)
+
+
+def convert_short_id(num):
+    """ Short ID converter - v3: Shuffled XOR UUID4 """
     mask_shuffle = 0x55555555555555555555555555555555
     mask_half = (1 << 64) - 1
     result = (num ^ (num >> 1)) & mask_shuffle
