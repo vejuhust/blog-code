7f210bff45eb7576a58eb99df76c0de0bc43f58b
Wei Ye <vejuhust@gmail.com>
2017-02-26 02:34:34 +0800

    Add short_id_v3.py

---
+++ python-short-id-generator/short_id_v3.py
@@ -0,0 +1,12 @@
+
+from uuid import uuid4
+from basehash import base62
+
+def generate_short_id():
+    """ Short ID generator - v3: Shuffled XOR UUID4 """
+    num = uuid4().int
+    mask_shuffle = 0x55555555555555555555555555555555
+    mask_half = (1 << 64) - 1
+    result = (num ^ (num >> 1)) & mask_shuffle
+    result = ((mask_half & (result >> 64)) << 1) | (mask_half & result)
+    return base62().encode(result)
