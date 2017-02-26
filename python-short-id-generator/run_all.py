837ff4255163d5da17b25e2ab5bab3f857a7c73c
Wei Ye <vejuhust@gmail.com>
2017-02-26 21:00:10 +0800

    Update run_all.py

--- python-short-id-generator/run_all.py
+++ python-short-id-generator/run_all.py
@@ -7,15 +7,16 @@ from short_id_v4 import generate_short_id as generate_short_id_v4
 from short_id_v5 import generate_short_id as generate_short_id_v5
 
 
-def print_id(tag, idstr):
-    print("{:>6s} | {:<30s} | {:^6d} |".format(tag, idstr, len(idstr)))
+def print_id(idstr, tag, doc):
+    print("{:>5s} | {:<50s} | {:<30s} | {:^6d} |".format(tag, doc, idstr, len(idstr)))
 
 
 def test_generate(idx):
     tag = "v{}".format(idx)
     name = "generate_short_id_{}".format(tag)
     if name in globals():
-        print_id(tag, globals()[name]())
+        func = globals()[name]
+        print_id(func(), tag, func.__doc__.strip())
 
 
 if __name__ == '__main__':
